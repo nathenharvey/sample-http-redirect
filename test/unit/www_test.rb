baafc00549c56cf1a6db6e4df706328eea7732c6
Nathen Harvey <nharvey@customink.com>
2011-06-18 13:30:48 -0700

    make the test fail

--- test/unit/www_test.rb
+++ test/unit/www_test.rb
@@ -7,7 +7,7 @@ class WwwCustominkRedirectTest < HTTPRedirectTest
   should_not_redirect "/"
 
   # legacy lab link
-  should_redirect "/cink/r.jsp?E=ci&F=t", :to => "/lab/"
+  should_redirect "/cink/r.jsp?E=ci&F=t", :to => "/lab/foo"
 
   # lab print locations
   should_redirect "/lab/help/print_locations", :to => "/lab/help/print"
